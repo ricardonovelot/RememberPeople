2657bb437b7c144f1ba56c31011332ed4684fc04
ricardonovelot <ricardonovelo97.tools@gmail.com>
2024-05-21 12:46:15 -0600

    Initial Commit

---
+++ RememberPeople/RememberPeopleApp.swift
@@ -0,0 +1,17 @@
+//
+//  RememberPeopleApp.swift
+//  RememberPeople
+//
+//  Created by Ricardo on 21/05/24.
+//
+
+import SwiftUI
+
+@main
+struct RememberPeopleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

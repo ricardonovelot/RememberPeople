cfacd838cb73a003f6a9ffd36c903356845462e4
ricardonovelot <ricardonovelo97.tools@gmail.com>
2024-05-23 00:53:02 -0600

    Commit

---
+++ RememberPeople/ContactForm.swift
@@ -0,0 +1,18 @@
+//
+//  ContactForm.swift
+//  RememberPeople
+//
+//  Created by Ricardo on 23/05/24.
+//
+
+import SwiftUI
+
+struct ContactForm: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ContactForm()
+}
---
+++ RememberPeople/EditContact.swift
@@ -0,0 +1,8 @@
+//
+//  EditContact.swift
+//  RememberPeople
+//
+//  Created by Ricardo on 22/05/24.
+//
+
+import Foundation

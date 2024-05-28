31c83e311ad0e6629453635b5a3537eb272068c3
ricardonovelot <ricardonovelo97.tools@gmail.com>
2024-05-28 14:46:21 -0600

    Commit

---
+++ RememberPeople/Features.swift
@@ -0,0 +1,39 @@
+//
+//  Features.swift
+//  RememberPeople
+//
+//  Created by Ricardo on 23/05/24.
+//
+ 
+
+/*
+
+ Feature Ideas:
+ 
+ 
+ 
+ 
+  - EmptyState
+  - Show currently selected tag
+  - palomita en tag seleccionada
+  - Open Keyboard inmediatly and select name when form
+  - How to delete tags?, edit tags names
+  - crop image
+  - maybe a click on a icon of the tag checks it and a click on the name of the tag opens the tag and the avillity to pin it
+  - what do i need to round up this app and publish to the app?
+  - user settings? icloud sync? onboarding?
+  - reduce spacing between sections of form
+  - name as navigationtitle
+  - see all tags en el form para que el form no se haga my largo
+  - delete button al final del form
+  - ask for review
+  - export as CVS
+  - Add person to IOS contacts and have a simbol that represents it is on your contacts app
+  - ios contacts app will backlink to the contact in this app via contact url
+  - repost a problem,
+  - suggest a feature
+  - Drag and drop, manually rearrange the list, moving the user from one section to another will update the date it was created
+
+ 
+
+*/

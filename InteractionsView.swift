c86b794ba4aa8b7204f3c6c15e39d9fe9f6be2b6
ricardonovelot <ricardonovelo97.tools@gmail.com>
2024-06-06 01:57:06 -0600

    Commit

---
+++ InteractionsView.swift
@@ -0,0 +1,18 @@
+//
+//  InteractionsView.swift
+//  RememberPeople
+//
+//  Created by Ricardo on 06/06/24.
+//
+
+import SwiftUI
+
+struct InteractionsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    InteractionsView()
+}

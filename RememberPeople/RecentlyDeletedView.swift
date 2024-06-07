35ae9f68a2978de851bc8cbc6802d8210988839d
ricardonovelot <ricardonovelo97.tools@gmail.com>
2024-06-07 02:54:11 -0600

    Commit

---
+++ RememberPeople/RecentlyDeletedView.swift
@@ -0,0 +1,18 @@
+//
+//  RecentlyDeletedView.swift
+//  RememberPeople
+//
+//  Created by Ricardo on 07/06/24.
+//
+
+import SwiftUI
+
+struct RecentlyDeletedView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RecentlyDeletedView()
+}
